f6850dfe27b596ce11e5b5b3644d6372e0b4bc1d
Harsha Vasireddy <harsha.vasireddy@coschool.ai>
2025-08-23 23:31:06 +0530

    Add theme support (light/dark mode)

--- login-microfrontend/src/main.tsx
+++ login-microfrontend/src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './theme'
+import { ChakraProvider } from '@chakra-ui/react'
+import { system } from './theme'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={theme}>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>,
