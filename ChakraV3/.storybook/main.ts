1ee94a0b89e4dbb3514b946935f9bb4a7f2977a2
Harsha Vasireddy <harsha.vasireddy@coschool.ai>
2025-09-20 20:33:26 +0530

    Fix Storybook dependencies and configuration for Node 20.18.0

--- ChakraV3/.storybook/main.ts
+++ ChakraV3/.storybook/main.ts
@@ -4,25 +4,18 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-viewport',
+    '@storybook/addon-controls',
+    '@storybook/addon-docs',
     '@storybook/addon-onboarding',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: true,
-        actions: true,
-        controls: true,
-        docs: true,
-        viewport: true,
-        toolbars: true,
-      },
-    },
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+
   },
   core: {
     disableTelemetry: true,
