72ec4dad9896f8447b2927dc8b230fd206224b3e
harsha7innerscore <harsha.vasireddy@coschool.ai>
2025-10-28 22:04:39 +0530

    refactor: Remove counter-related logic and fix React import warning

--- redux/redux-app/src/App.js
+++ redux/redux-app/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
--- redux/redux-app/src/app/store.js
+++ redux/redux-app/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import educationReducer from '../features/education/educationSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     education: educationReducer,
   },
 });
